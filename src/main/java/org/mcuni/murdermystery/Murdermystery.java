e25a939cd9f6580a12b81f0c0bcbad06eb6ae8fa
lewmilburn <lewiscmilburn@gmail.com>
2022-10-25 23:24:12 +0100

    Initial commit

---
+++ src/main/java/org/mcuni/murdermystery/Murdermystery.java
@@ -0,0 +1,17 @@
+package org.mcuni.murdermystery;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Murdermystery extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
